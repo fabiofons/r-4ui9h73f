2291747554426f7a9639baf7581747d280730bdd
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-07-16 18:26:16 -0500

    fixing the code for pass the test 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {text:''};
+
+    this.repeat = this.repeat.bind(this);
+  }
+
+  repeat(e){
+    this.setState({text: e.target.value})
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repeat}/>
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
